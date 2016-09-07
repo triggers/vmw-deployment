86593ecf7b4c1ee262160c70d3cb96442956d681
Richard Potter <potter@axsh.net>
2016-09-07 19:47:30 +0900

    initial: added template for www/config-dispatch.sh

---
+++ www/config-dispatch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "tested at $(date)"
+
+rm keep-polling  # stop script from being called again until the next VM boot
+
+#end-of-script#  # <<-minimal check to make sure whole script was downloaded
+
+
