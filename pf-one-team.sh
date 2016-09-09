a2b6f0638eb6fc72ea85a7ea5b3449649042f63f
Richard Potter <potter@axsh.net>
2016-09-09 13:03:39 +0900

    Added pf-one-team.sh, untested

---
+++ pf-one-team.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# ROUGH PROTOTYPE, not generlized...
+
+failed()
+{
+    echo  "Failed...exiting: $@" 1>&2
+    exit 255
+}
+
+teamnumber="$1"
+
+[[ "$teamnumber" == [0-9] ]] && teamnumber=0$teamnumber   # make the UI a little nicer
+[[ "$teamnumber" == [0-9][0-9] ]] || failed "Parameter must be two digit team number"
+
+IP=192.168.100.$(( 10#$teamnumber ))  # make sure leading zero does not switch to bash octal mode
+
+# 2ttvv  tt=team number   vv=vm number  (from 11 12 21 22)
+# e.g.  20101 = team 1 vm 11
+
+pfparams="$(
+
+  # e.g.: 20422 goes to team4's wakame2 VM
+  for i in 11 12 21 22; do
+      aport="2${teamnumber}${i}"
+      echo -n  " -L ${aport}:192.168.4.${i}:22"
+  done
+
+)"
+
+ssh root@"$IP" $pfparams -g  sleep 999${teamnumber}
