4d73644dc129c0dd1bdda88ab42c85b2e958872c
Richard Potter <potter@axsh.net>
2016-09-10 18:26:45 +0900

    Refactored net setup code to use esxcli via ssh to esxi

--- deploy-one-team.sh
+++ deploy-one-team.sh
@@ -15,15 +15,15 @@ teamnumber="$1"
     
 eee='esxcli -s 192.168.1.219 -u root -d 1A:A8:3F:CE:AB:D1:31:11:0C:7A:CB:3C:1D:1E:2D:E8:72:DE:99:EC -p Wakame4Axsh'
 
-eee() { $eee "$@" ; }
+ssh_vswitch_cmd() { ssh root@192.168.1.219 esxcli network vswitch standard "$@" ; }
 
-eee() { echo $eee "$@" ; }  # disable for now
+# ssh_vswitch_cmd() { echo  "$@" ; }  # disable for now
 
 for i in 1 2 3; do
-    eee network vswitch standard add -v t${teamnumber}-net${i}
-    eee network vswitch standard portgroup add -p t02-net1-pg -v t${teamnumber}-net${i}
+    ssh_vswitch_cmd add -v t${teamnumber}-net${i}
+    ssh_vswitch_cmd portgroup add -p t02-net1-pg -v t${teamnumber}-net${i}
 done
-
+exit
 time ovftool --name="gw${teamnumber}" --datastore="ahd" \
      --net:"VM Network"="class-net-pg" --net:"pg1"="t${teamnumber}-net1-pg" \
      -dm=thin /root/ovftool/centos68-x86-autoconf16.ovf vi://root:Wakame4Axsh@192.168.1.219
