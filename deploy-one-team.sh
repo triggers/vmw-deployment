c367904997322ead7b67b336803f44f9b4ce0ab8
Richard Potter <potter@axsh.net>
2016-09-10 23:28:44 +0900

    Removed debugging lines from deploy-one-team.sh

--- deploy-one-team.sh
+++ deploy-one-team.sh
@@ -13,13 +13,9 @@ teamnumber="$1"
 [[ "$teamnumber" == [0-9] ]] && teamnumber=0$teamnumber   # make the UI a little nicer
 [[ "$teamnumber" == [0-9][0-9] ]] || failed "Parameter must be two digit team number"
     
-eee='esxcli -s 192.168.1.219 -u root -d 1A:A8:3F:CE:AB:D1:31:11:0C:7A:CB:3C:1D:1E:2D:E8:72:DE:99:EC -p Wakame4Axsh'
-
 ssh_vswitch_cmd() { ssh root@192.168.1.219 esxcli network vswitch standard "$@" ; }
 
-# ssh_vswitch_cmd() { echo  "$@" ; }  # disable for now
-
-false && for i in 1 2 3 4 5; do
+for i in 1 2 3 4 5; do
     echo
     echo " --- $i ---"
     ssh_vswitch_cmd add -v t${teamnumber}-net${i}
@@ -33,21 +29,19 @@ time ovftool --name="gw${teamnumber}" --datastore="ahd" \
      --net:"t02-net4-pg"="t${teamnumber}-net4-pg" \
      -dm=thin /root/ovftool/centos68-x86-autoconf16-3nic.ovf vi://root:Wakame4Axsh@192.168.1.219
 
-# exit # STILL TESTING!
-
-false && for i in 01 02; do
+for i in 01 02; do
     time ovftool --name="t${teamnumber}-vm${i}" --datastore="ahd" \
 	 --net:"VM Network"="t${teamnumber}-net2-pg" --net:"pg1"="t${teamnumber}-net1-pg" \
 	 -dm=thin /root/ovftool/centos68-x86-autoconf16.ovf vi://root:Wakame4Axsh@192.168.1.219
 done
 
-false && for i in 03 04; do
+for i in 03 04; do
     time ovftool --name="t${teamnumber}-vm${i}" --datastore="ahd" \
 	 --net:"VM Network"="t${teamnumber}-net3-pg" --net:"pg1"="t${teamnumber}-net1-pg" \
 	 -dm=thin /root/ovftool/centos68-x86-autoconf16.ovf vi://root:Wakame4Axsh@192.168.1.219
 done
 
-false && for i in 05 06 07; do
+for i in 05 06 07; do
     time ovftool --name="t${teamnumber}-vm${i}" --datastore="ahd" \
 	 --net:"VM Network"="t${teamnumber}-net4-pg" --net:"pg1"="t${teamnumber}-net5-pg" \
 	 -dm=thin /root/ovftool/centos68-x86-autoconf16.ovf vi://root:Wakame4Axsh@192.168.1.219
